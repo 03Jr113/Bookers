f8317bf628a382379ff8cbb339b5fb4850ac8eb5
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-08-09 05:51:59 +0000

    [Add]UpdateSuccessMSG

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully updated."
   end
 
   def destroy
