e8b7a1f8e4da44d0ae23702c640b5de21d79d174
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-08-09 05:31:04 +0000

    [Add]successMessage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,19 @@
 class BooksController < ApplicationController
   
-  def new
-    @book = Book.new
-  end
-
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -38,7 +37,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title,:body)
   end
   
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
   config.hosts << "3b633c437ebc4e5691fc51d73738d902.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.web_console.allowed_ips = '123.1.19.148'
 end
